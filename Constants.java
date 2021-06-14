4373d1401001b165cf7d05d10f02564576f53756
Kostas Koniavitis <84277223+KostasKoniavitis@users.noreply.github.com>
2021-06-14 22:26:07 +0300

    Delete Constants.java

--- Constants.java
+++
@@ -1,8 +0,0 @@
-package com.example.newsapp;
-
-public class Constants {
-    public static final int NEWS_LOADER_ID = 1;
-
-    public final static String GUARDIANAPIURL = "https://content.guardianapis.com/search?show-fields=trailText&show-tags=contributor";
-    public final static String APIKEY = "test";
-}
